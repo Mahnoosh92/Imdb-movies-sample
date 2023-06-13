32aa3ee543e73b1b141835c809ade76de7300904
mahnoosh92 <mahnooshmahdavi2012@gmail.com>
2023-06-13 12:04:07 +0100

    changing navigation

---
+++ authentication/src/test/java/com/mahnoosh/athentication/ui/signinWithEmailAndPassword/SignInWithEmailAndPasswordViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.mahnoosh.athentication.ui.signinWithEmailAndPassword
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SignInWithEmailAndPasswordViewModelTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+}
